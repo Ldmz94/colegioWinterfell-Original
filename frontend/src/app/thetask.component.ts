122fe798f7dd9ac9b0cb6084ac622f7c40f49638
julianaramirezc <julianaramirez66@gmail.com>
2018-07-13 17:13:21 -0500

    Validaciones de agregar tareas

--- frontend/src/app/thetask.component.ts
+++ frontend/src/app/thetask.component.ts
@@ -8,11 +8,10 @@ import { ApiService } from './api.service'
 })
 //Solo para pruebas de conexión y de mostrar mensajes
 export class theTaskComponent {
+  minDate = new Date();
   gradeName: string;
-  subjectName : string;
-  taskData = {};
-
-
+  subjectName: string;
+  taskData: any = {};
 
   constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {
     let va = this.activatedRoute.params.subscribe(params => {
@@ -27,17 +26,25 @@ export class theTaskComponent {
   }
 
   post() {
-    var obj = {taskData :{}, gradeName:'', courseName:'', subjectName: ''}
+    var obj = { taskData: {}, gradeName: '', courseName: '', subjectName: '' }
     let tempCourse = this.subjectName.split(">");
     //this.taskData.subject = tempCourse[1]
     obj.taskData = this.taskData
-    obj.gradeName = (this.gradeName)
-    obj.courseName =(tempCourse[0])
-    obj.subjectName = (tempCourse[1])
-    console.log("1: ", obj);
-    this.apiService.addTask(obj);
+    if (this.taskData.archivo.endsWith('.com') ) {
+      obj.gradeName = (this.gradeName)
+      obj.courseName = (tempCourse[0])
+      obj.subjectName = (tempCourse[1])
+      console.log("1: ", obj);
+      this.apiService.addTask(obj);
+      alert("Agregó Tarea");
+    } else {
+      alert("Este enlace no es válido");
+    }
+      
+
   }
 
-  
+
+
 }
 
